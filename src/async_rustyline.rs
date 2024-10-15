fa5667c2fe0dc566575dd911da9b7086dc2432e2
Marco Neumann <marco@crepererum.net>
2024-10-15 12:04:03 +0200

    feat: fs history

--- src/async_rustyline.rs
+++ src/async_rustyline.rs
@@ -1,37 +1,54 @@
-use std::sync::Arc;
+use std::{path::PathBuf, sync::Arc};
 
 use anyhow::{Context, Result};
-use rustyline::{error::ReadlineError, history::MemHistory, Config, Editor};
+use directories::ProjectDirs;
+use rustyline::{error::ReadlineError, history::FileHistory, Config, Editor};
 use tokio::sync::Mutex;
 
-type MyEditor = Editor<(), MemHistory>;
+type MyEditor = Editor<(), FileHistory>;
 
 pub(crate) struct AsyncRustyline {
     inner: Arc<Mutex<MyEditor>>,
+    path: Option<PathBuf>,
 }
 
 impl AsyncRustyline {
     pub(crate) async fn new() -> Result<Self> {
-        let inner = tokio::task::spawn_blocking(|| {
+        let (inner, path) = tokio::task::spawn_blocking(|| {
+            let path =
+                ProjectDirs::from("net.crepererum", "crepererum.net", env!("CARGO_PKG_NAME"))
+                    .map(|dirs| dirs.cache_dir().join("history.txt"));
+
             let cfg = Config::builder().build();
-            let history = MemHistory::new();
-            MyEditor::with_history(cfg, history)
+            let history = FileHistory::new();
+            let mut inner = MyEditor::with_history(cfg, history).context("setup rustyline")?;
+            if let Some(path) = &path {
+                if std::fs::exists(path).ok().unwrap_or_default() {
+                    inner.load_history(path).context("load history")?;
+                }
+            }
+
+            Ok((inner, path)) as Result<_, anyhow::Error>
         })
         .await
-        .context("tokio spawn")?
-        .context("setup rustyline")?;
+        .context("tokio spawn")??;
 
         Ok(Self {
             inner: Arc::new(Mutex::new(inner)),
+            path,
         })
     }
 
     pub(crate) async fn read_line(&self) -> Result<Option<String>> {
         let mut inner = Arc::clone(&self.inner).lock_owned().await;
+        let path = self.path.clone();
 
         let rl_res = tokio::task::spawn_blocking(move || {
             let s = inner.readline(">> ")?;
             inner.add_history_entry(&s)?;
+            if let Some(path) = path {
+                inner.save_history(&path)?;
+            }
             Ok(s)
         })
         .await
