41648e8dd6d5871fd07400640e2bb91e1db51e1f
Marco Neumann <marco@crepererum.net>
2024-10-30 13:14:00 +0100

    fix: create cache dir

--- src/async_rustyline.rs
+++ src/async_rustyline.rs
@@ -15,18 +15,25 @@ pub(crate) struct AsyncRustyline {
 impl AsyncRustyline {
     pub(crate) async fn new() -> Result<Self> {
         let (inner, path) = tokio::task::spawn_blocking(|| {
-            let path =
-                ProjectDirs::from("net.crepererum", "crepererum.net", env!("CARGO_PKG_NAME"))
-                    .map(|dirs| dirs.cache_dir().join("history.txt"));
-
             let cfg = Config::builder().build();
             let history = FileHistory::new();
             let mut inner = MyEditor::with_history(cfg, history).context("setup rustyline")?;
-            if let Some(path) = &path {
-                if std::fs::exists(path).ok().unwrap_or_default() {
-                    inner.load_history(path).context("load history")?;
+
+            let cache_dir =
+                ProjectDirs::from("net.crepererum", "crepererum.net", env!("CARGO_PKG_NAME"))
+                    .map(|dirs| dirs.cache_dir().to_owned());
+            let path = match cache_dir {
+                Some(cache_dir) => {
+                    std::fs::create_dir_all(&cache_dir).context("create cache dir")?;
+
+                    let path = cache_dir.join("history.txt");
+                    if std::fs::exists(&path).ok().unwrap_or_default() {
+                        inner.load_history(&path).context("load history")?;
+                    }
+                    Some(path)
                 }
-            }
+                None => None,
+            };
 
             Ok((inner, path)) as Result<_, anyhow::Error>
         })
